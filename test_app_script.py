06908cbc38bf688ce9a13fb91dbe451f65fadf82
andrewdevilliers <54674639+andrewdevilliers@users.noreply.github.com>
2020-06-25 22:27:13 +0200

    Update test_app_script.py

--- test_app_script.py
+++ test_app_script.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 st.title("This is a test")
-number_1 = st.text_input("First number: ")
-number_2 = st.text_input("Second number: ")
+number_1 = st.number_input("First number: ")
+number_2 = st.number_input("Second number: ")
 result = number_1 + number_2
-st.write("The sum of your numbers is: " + result)
+st.write("The sum of your numbers is: ", result)
+
